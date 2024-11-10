69767fb4cef2e62d097ecc9a6c5166b36283b366
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-11-10 20:40:51 +0200

    delete_error

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,18 +1,16 @@
 import { useSelector } from 'react-redux';
-import { selectError, selectLoading } from '../../redux/auth/selectors';
+import { selectLoading } from '../../redux/auth/selectors';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import Loader from '../../components/Loader/Loader';
 import css from './LoginPage.module.css';
 
 export default function LoginPage() {
   const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
 
   return (
     <div>
       <h2 className={css.textLogin}>Sign in to your account!</h2>
       {loading && <Loader />}
-      {error && <p>{error}</p>}
       <LoginForm />
     </div>
   );
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { selectError, selectLoading } from '../../redux/auth/selectors';
+import { selectLoading } from '../../redux/auth/selectors';
 
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 import Loader from '../../components/Loader/Loader';
@@ -7,13 +7,11 @@ import css from './RegistrationPage.module.css';
 
 export default function RegistrationPage() {
   const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
 
   return (
     <div>
       <h2 className={css.textRegister}>Registration new user!</h2>
       {loading && <Loader />}
-      {error && <p>{error}</p>}
       <RegistrationForm />
     </div>
   );
